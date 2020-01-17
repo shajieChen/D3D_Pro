56142cab9ce546e97100dd8cd09cdb8d17c119d1
shajie <s3582098@student.rmit.edu.au>
2020-01-17 11:43:59 +0800

    添加基本的shader 包括简单的颜色输出 和纹理颜色输出

---
+++ D3D_Pro/Project1/basicVS.hlsl
@@ -0,0 +1,25 @@
+cbuffer cbPerFrame : register(b0)
+{
+	float4x4 matVP; 
+	float4x4 matGeo; 
+};
+
+struct VSInput
+{
+	float3 Position : POSITION ;
+	float4 Color : COLOR;
+};
+
+struct VSOutput
+{
+	float4 Position : SV_POSITION;
+	float4 Color : COLOR; 
+};
+
+VSOutput main(VSInput vin)
+{
+	VSOutput vout = (VSOutput) 0 ; 
+	vout.Color = float4(1.0f, 1.0f, 1.0f, 1.0f); 
+	vout.Position = mul(mul(float4(vin.Position ,1.0f) , matGeo), matVP); 
+	return vout;
+}
